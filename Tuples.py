7cadb9cd3eab64d8161dcf52ad1d4928dbc120ae
Helen Agu <115573147+Helen-Agu@users.noreply.github.com>
2023-02-05 20:06:58 +0100

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,26 +1,41 @@
 print("Hello, El!")
+Output
+Hello, El!
 
 # Creating tuples
 tuple1 = ('Tola', 'Nkechi', 'Deji', 
           'Ronke', 'Onyedika', 'Olaedo')
 tuple2 = ('Bose', 'Amaka', 'Mustapha')
 
+
 # joining 2 tuples - Concatenation operator (+)
 print(tuple1 + tuple2)
+Output
+('Tola', 'Nkechi', 'Deji', 'Ronke', 'Onyedika', 'Olaedo', 'Bose', 'Amaka', 'Mustapha') 
 
 # membership operator
 print('Bose' in tuple1)
 print('Tola' not in tuple2)
+Output
+False
+True
 
 # number of items in the tuple
 print(len(tuple1))
 print(len(tuple2))
+Output
+6
+3
 
 # Repetition operator (*)
 print(tuple2*3)
+Output
+('Bose', 'Amaka', 'Mustapha', 'Bose', 'Amaka', 'Mustapha', 'Bose', 'Amaka', 'Mustapha')
 
 # index
 print (tuple1.index('Nkechi'))
 print (tuple1.index('Olaedo'))
 print (tuple2.index('Mustapha'))
-
+Output
+1
+5
